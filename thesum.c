6c872190f576fa862335aaa73f2c774b652ab9ef
tonyiovino <antonioiovino2004tony@gmail.com>
2020-03-26 16:11:16 +0000

    TheSum (Incompleto, miglioramenti estetici)

--- thesum.c
+++ thesum.c
@@ -8,6 +8,11 @@ int main()
 
 	
 	f = fopen("sum.dat","r");
+	fscanf(f, "%d", &f_num);
+
+	printf("Lettura dal file sum.dat in corso...\n");
+	printf("Valore corrente: %d\n\n", f_num);
+
 
 	if(f) {
 		FILE *f = fopen("sum.dat","r");
@@ -21,13 +26,19 @@ int main()
 
 	fclose(f);
 
-	printf("Inserisca un numero: ");
+	printf("Inserisca il valore da sommare: ");
 	scanf("%d", &num);
 
 	f_num = num + f_num;
 
 	f = fopen("sum.dat","w");
 	fprintf(f, "%d\n", f_num);
+
+	fscanf(f, "%d", &f_num);
+
+	printf("\nHo letto il valore %d\n", f_num);
+
+	printf("Salvataggio nel file sum.dat in corso...\n");
 
 	fclose(f);
 
